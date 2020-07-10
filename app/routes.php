82f95efa674f74b90ff3f8fa53e9868d2988a796
JulianFun123 <julianfun123@yahoo.com>
2020-07-11 00:07:21 +0200

    Added CLI

--- app/routes.php
+++ app/routes.php
@@ -5,7 +5,7 @@ $templateDirectory = "resources/views/templates/";
 $router->get("/", "homepage.php");
 $router->get("/example", "!ExampleController@example");
 $router->get("/example/users", "!ExampleController@exampleJSONAPI");
-$router->get("/example/adusers", "!ExampleController@addUser");
+$router->post("/example/users", "!ExampleController@addUser");
 
 $router->get("/d/([a-z]*)", "customtest.php");
 
